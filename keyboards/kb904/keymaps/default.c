de6dafbf1b29f61affbcf12644cc1860dfe8bf84
hank <hank.xiong@inateck.com>
2026-03-19 20:59:36 +0800

    fix(kb904): correct LAYER_BASE keymap with standard keycode aliases
    - Replace 0x00 with KC_NO for consistency
    - Use standard keycode aliases from keycodes.h:
      - Modifiers: KC_LALT, KC_RALT, KC_LCTL, KC_LSFT, KC_RSFT, KC_LGUI, KC_RGUI
      - Symbols: KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, KC_SCLN, KC_QUOT, KC_COMM, KC_SLSH
      - Functions: KC_GRV, KC_CAPS, KC_DEL, KC_BSPC, KC_ENT, KC_SPC
      - Arrows: KC_LEFT, KC_RGHT, KC_UP, KC_DOWN
    - Complete each row to 16 columns
    - Add original column mapping comment for reference

--- keyboards/kb904/keymaps/default.c
+++ keyboards/kb904/keymaps/default.c
@@ -58,31 +58,16 @@ const uint16_t keymap_layers[LAYER_MAX][MATRIX_ROWS][MATRIX_COLS] = {
     // LAYER_BASE - Windows/Android 基础层
     // =====================================================
     [LAYER_BASE] = {
-        // 新col:    0       1       2       3       4       5       6       7       8       9       10      11      12      13      14      15
-        // 原col:    1       2       3       4       5       6       7       14      12      16      0       9       8       10      15      11
-        /* Row 0 */  KC_Q,   KC_W,   KC_E,   KC_R,   KC_U,   KC_I,   KC_O,   KC_NO,  KC_P,   KC_BSLS,KC_GRV, KC_NO,  KC_LBRC,KC_NO,  KC_NO,  KC_SPC,
-        /* Row 1 */  KC_TAB, KC_NO,  KC_NO,  KC_T,   KC_Y,   KC_RBRC,KC_F7,  KC_NO,  KC_NO,  KC_BSPC,KC_NO,  KC_NO,  KC_LBRC,KC_NO,  KC_LSFT,KC_NO,
-        /* Row 2 */  KC_A,   KC_S,   KC_D,   KC_F,   KC_J,   KC_K,   KC_L,   KC_F3,  KC_SCLN,KC_NO,  KC_NO,  KC_LCTL,KC_SCLN,KC_NO,  KC_RSFT,KC_NO,
-        /* Row 3 */  KC_ESC, KC_NO,  KC_NO,  KC_G,   KC_H,   KC_F6,  KC_NO,  KC_UP,  KC_QUOT,KC_NO,  KC_NO,  KC_LGUI,KC_QUOT,KC_NO,  KC_NO,  KC_NO,
-        /* Row 4 */  KC_Z,   KC_X,   KC_C,   KC_V,   KC_M,   KC_COMM,KC_DOT, KC_NO,  KC_NO,  KC_ENT, KC_RALT,KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-        /* Row 5 */  KC_NO,  KC_NO,  KC_NO,  KC_B,   KC_N,   KC_NO,  KC_NO,  KC_LEFT,KC_SLSH,KC_NO,  KC_NO,  KC_RGUI,KC_SLSH,KC_NO,  KC_F11, KC_DOWN,
-        /* Row 6 */  KC_GRV, KC_F1,  KC_F2,  KC_5,   KC_6,   KC_EQL, KC_F8,  KC_NO,  KC_MINS,KC_F9,  KEYMAP_FN_KEY,KC_NO,  KC_MINS,KC_F9,  KC_NO,  KC_NO,
-        /* Row 7 */  KC_1,   KC_2,   KC_3,  KC_4,   KC_7,   KC_8,   KC_9,   KC_NO,  KC_0,   KC_NO,  KC_F5,  KC_F12, KC_0,   KC_NO,  KC_F10, KC_NO,
-    },
-
-    // =====================================================
-    // LAYER_FN - Fn 功能层 (按住 Fn 激活)
-    // =====================================================
-    [LAYER_FN] = {
-        // 新col:    0       1       2       3       4       5       6       7       8       9       10      11      12      13      14      15
-        /* Row 0 */  KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_NO,  KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-        /* Row 1 */  KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_F7,  KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-        /* Row 2 */  KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_F3,  KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_RSFT,KC_TRNS,
-        /* Row 3 */  KC_ESC, KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_F6,  KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-        /* Row 4 */  KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_ENT, KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-        /* Row 5 */  KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_RGUI,KC_TRNS,KC_TRNS,KC_F11, KC_TRNS,
-        /* Row 6 */  KC_TRNS,KC_F1,  KC_F2,  KC_TRNS,KC_TRNS,KC_TRNS,KC_F8,  KC_TRNS,KC_TRNS,KC_F9,  KC_TRNS,KC_TRNS,KC_TRNS,KC_F9,  KC_TRNS,KC_TRNS,
-        /* Row 7 */  KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_F5,  KC_F12, KC_TRNS,KC_TRNS,KC_F10, KC_TRNS,
+        // 新col:    0        1        2        3        4        5        6        7        8        9        10       11       12       13       14       15
+        // 原col:    1        2        3        4        5        6        7        14       12       16       0        9        8        10       15       11
+        /* Row 0 */ KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+        /* Row 1 */ KC_NO,    KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_NO,    KC_LALT,  KC_NO,    KC_NO,    KC_NO,    KC_NO,
+        /* Row 2 */ KC_TAB,   KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_NO,    KC_NO,    KC_LCTL,  KC_NO,
+        /* Row 3 */ KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_NO,    KC_NO,    KC_NO,    KC_RSFT,  KC_NO,    KC_NO,
+        /* Row 4 */ KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_LSFT,  KC_NO,    KC_NO,
+        /* Row 5 */ KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KEYMAP_FN_KEY, KC_NO, KC_NO,    KC_NO,
+        /* Row 6 */ KC_F10,   KC_F11,   KC_MINS,  KC_EQL,   KC_P,     KC_LBRC,  KC_SCLN,  KC_COMM,  KC_RSFT,  KC_RALT,  KC_UP,    KC_DOWN,  KC_NO,    KC_LGUI,  KC_NO,    KC_NO,
+        /* Row 7 */ KC_F12,   KC_DEL,   KC_BSPC,  KC_RBRC,  KC_BSLS,  KC_QUOT,  KC_ENT,   KC_SLSH,  KC_LEFT,  KC_RGHT,  KC_NO,    KC_SPC,   KC_NO,    KC_NO,    KC_RGUI,  KC_NO,
     },
 
     // =====================================================
